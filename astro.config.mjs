dc9b424ca18797d29988c46307294c24f0b6192f
Martijn Bokma <mail@martijnbokma.nl>
2025-03-11 16:17:35 +0100

    Update base URL configuration for root deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://couch-docs.github.io',
-	base: '/couch-docs',
+	base: '/',
 	integrations: [
 		starlight({
 			// plugins: [starlightThemeObsidian()],
